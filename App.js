e8b5a0c206556abd165f0874b3f716d4bc4356d5
krissanawat <krissnawat@hey.com>
2020-08-13 13:16:59 +0700

    initial commit

---
+++ App.js
@@ -0,0 +1,273 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, { Component } from 'react';
+import AudioRecorderPlayer, {
+  AVEncoderAudioQualityIOSType,
+  AVEncodingOption,
+  AudioEncoderAndroidType,
+  AudioSet,
+  AudioSourceAndroidType,
+} from 'react-native-audio-recorder-player';
+import {
+  PermissionsAndroid,
+  Platform, View, Text, StyleSheet
+} from 'react-native';
+import Background from './Background';
+import Logo from './Logo';
+import Header from './Header';
+import Button from './Button';
+import { theme } from './core/theme';
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggingIn: false,
+      recordSecs: 0,
+      recordTime: '00:00:00',
+      currentPositionSec: 0,
+      currentDurationSec: 0,
+      playTime: '00:00:00',
+      duration: '00:00:00',
+    };
+    this.audioRecorderPlayer = new AudioRecorderPlayer();
+    this.audioRecorderPlayer.setSubscriptionDuration(0.09); // optional. Default is 0.1
+  }
+
+  render() {
+
+    return (
+      <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center', alignContent: 'center' }}>
+        <Background>
+          <Logo />
+          <Header>InstaPlayer</Header>
+          <Text style={styles.txtRecordCounter} >{this.state.recordTime}</Text>
+
+          <Button mode="contained" icon="record" onPress={() => this.onStartRecord}>
+            RECORD
+        </Button>
+          <Button
+            icon="stop"
+            mode="outlined"
+            onPress={() => this.onStopRecord}
+          >
+            STOP
+    </Button>
+          <Button mode="contained" icon="play" onPress={() => this.onStartPlay}>
+            PLAY
+        </Button>
+
+          <Button
+            icon="pause"
+            mode="contained"
+            onPress={() => this.onPausePlay}
+          >
+            PAUSE
+    </Button>
+          <Button
+            icon="stop"
+            mode="outlined"
+            onPress={() => this.onStopPlay}
+          >
+            STOP
+    </Button>
+        </Background>
+      </View>
+    )
+  }
+  onStatusPress = (e) => {
+    const touchX = e.nativeEvent.locationX;
+    console.log(`touchX: ${touchX}`);
+    const playWidth =
+      (this.state.currentPositionSec / this.state.currentDurationSec) *
+      (screenWidth - 56 * ratio);
+    console.log(`currentPlayWidth: ${playWidth}`);
+
+    const currentPosition = Math.round(this.state.currentPositionSec);
+    console.log(`currentPosition: ${currentPosition}`);
+
+    if (playWidth && playWidth < touchX) {
+      const addSecs = Math.round(currentPosition + 1000);
+      this.audioRecorderPlayer.seekToPlayer(addSecs);
+      console.log(`addSecs: ${addSecs}`);
+    } else {
+      const subSecs = Math.round(currentPosition - 1000);
+      this.audioRecorderPlayer.seekToPlayer(subSecs);
+      console.log(`subSecs: ${subSecs}`);
+    }
+  };
+  onStartRecord = async () => {
+    if (Platform.OS === 'android') {
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+          {
+            title: 'Permissions for write access',
+            message: 'Give permission to your storage to write a file',
+            buttonPositive: 'ok',
+          },
+        );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          console.log('You can use the storage');
+        } else {
+          console.log('permission denied');
+          return;
+        }
+      } catch (err) {
+        console.warn(err);
+        return;
+      }
+    }
+    if (Platform.OS === 'android') {
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+          {
+            title: 'Permissions for write access',
+            message: 'Give permission to your storage to write a file',
+            buttonPositive: 'ok',
+          },
+        );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          console.log('You can use the camera');
+        } else {
+          console.log('permission denied');
+          return;
+        }
+      } catch (err) {
+        console.warn(err);
+        return;
+      }
+    }
+    const path = Platform.select({
+      ios: 'hello.m4a',
+      android: 'sdcard/hello.mp4',
+    });
+    const audioSet = {
+      AudioEncoderAndroid: AudioEncoderAndroidType.AAC,
+      AudioSourceAndroid: AudioSourceAndroidType.MIC,
+      AVEncoderAudioQualityKeyIOS: AVEncoderAudioQualityIOSType.high,
+      AVNumberOfChannelsKeyIOS: 2,
+      AVFormatIDKeyIOS: AVEncodingOption.aac,
+    };
+    console.log('audioSet', audioSet);
+    const uri = await this.audioRecorderPlayer.startRecorder(path, audioSet);
+    this.audioRecorderPlayer.addRecordBackListener((e) => {
+      this.setState({
+        recordSecs: e.current_position,
+        recordTime: this.audioRecorderPlayer.mmssss(
+          Math.floor(e.current_position),
+        ),
+      });
+    });
+    console.log(`uri: ${uri}`);
+  };
+
+  onStopRecord = async () => {
+    const result = await this.audioRecorderPlayer.stopRecorder();
+    this.audioRecorderPlayer.removeRecordBackListener();
+    this.setState({
+      recordSecs: 0,
+    });
+    console.log(result);
+  };
+
+  onStartPlay = async (e) => {
+    console.log('onStartPlay');
+    const path = Platform.select({
+      ios: 'hello.m4a',
+      android: 'sdcard/hello.mp4',
+    });
+    const msg = await this.audioRecorderPlayer.startPlayer(path);
+    this.audioRecorderPlayer.setVolume(1.0);
+    console.log(msg);
+    this.audioRecorderPlayer.addPlayBackListener((e) => {
+      if (e.current_position === e.duration) {
+        console.log('finished');
+        this.audioRecorderPlayer.stopPlayer();
+      }
+      this.setState({
+        currentPositionSec: e.current_position,
+        currentDurationSec: e.duration,
+        playTime: this.audioRecorderPlayer.mmssss(
+          Math.floor(e.current_position),
+        ),
+        duration: this.audioRecorderPlayer.mmssss(Math.floor(e.duration)),
+      });
+    });
+  };
+
+  onPausePlay = async (e) => {
+    await this.audioRecorderPlayer.pausePlayer();
+  };
+
+  onStopPlay = async (e) => {
+    console.log('onStopPlay');
+    this.audioRecorderPlayer.stopPlayer();
+    this.audioRecorderPlayer.removePlayBackListener();
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#455A64',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  titleTxt: {
+    marginTop: 100,
+    color: 'white',
+    fontSize: 28,
+  },
+  viewRecorder: {
+    marginTop: 40,
+    width: '100%',
+    alignItems: 'center',
+  },
+  recordBtnWrapper: {
+    flexDirection: 'row',
+  },
+  viewPlayer: {
+    marginTop: 60,
+    alignSelf: 'stretch',
+    alignItems: 'center',
+  },
+  viewBarWrapper: {
+    marginTop: 28,
+    marginHorizontal: 28,
+    alignSelf: 'stretch',
+  },
+  viewBar: {
+    backgroundColor: '#ccc',
+    height: 4,
+    alignSelf: 'stretch',
+  },
+  viewBarPlay: {
+    backgroundColor: 'white',
+    height: 4,
+    width: 0,
+  },
+  playStatusTxt: {
+    marginTop: 8,
+    color: '#ccc',
+  },
+
+  txtCounter: {
+    marginTop: 12,
+    color: 'white',
+    fontSize: 20,
+    textAlignVertical: 'center',
+    fontWeight: '200',
+    fontFamily: 'Helvetica Neue',
+    letterSpacing: 3,
+  },
+});
+
+export default App;
